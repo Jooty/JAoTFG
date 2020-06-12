c8aef892ec230bb6c3c18f14aa366f58f02be01e
Jooty <compundbread@gmail.com>
2020-06-12 08:36:45 -0400

    Changed gas reel speed from 1.15f to 1.5f.

--- JAoTFG/Assets/Scripts/Game/GameVariables.cs
+++ JAoTFG/Assets/Scripts/Game/GameVariables.cs
@@ -15,7 +15,7 @@
     public const bool MG_RETRACT_ON_GAS = true;
     public const float MG_HOOK_RANGE = 50f;
     public const float MG_HOOK_MAX_RUNAWAY_RANGE = MG_HOOK_RANGE * 1.5f;
-    public const float MG_GAS_REEL_SPEED_MULTIPLIER = 1.15f;
+    public const float MG_GAS_REEL_SPEED_MULTIPLIER = 1.5f;
 
     // TITAN
     public const int TITAN_DISSIPATE_TIMER = 30;
