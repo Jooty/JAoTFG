1acfbdfbeab94fb612b1ada5a775734066a7e64b
Jooty <compundbread@gmail.com>
2020-06-18 05:50:13 -0400

    Added footstep sounds for humans.

---
+++ JAoTFG/Assets/Scripts/Scriptables/CharacterAnimationEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEvents : MonoBehaviour
+{
+
+    private AudioClip[] footstepSounds;
+
+    // locals
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+
+        footstepSounds = Resources.LoadAll<AudioClip>("SFX/FootSteps/Grass/");
+    }
+
+    public void Footstep()
+    {
+        aud.PlayOneShot(footstepSounds[Random.Range(0, footstepSounds.Length)]);
+    }
+
+}
