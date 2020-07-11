7ea127c333d0fe4d2bb2255bd8c1efa0611237cd
Jooty <compundbread@gmail.com>
2020-07-11 01:52:32 -0400

    Added utility class.

---
+++ JAoTFG/Assets/Scripts/Utilities/Util.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public static class Util
+{
+
+    public static byte[] HexToByte(string hex)
+    {
+        byte[] data = new byte[hex.Length / 2];
+        for (int i = 0; i < hex.Length; i += 2)
+        {
+            data[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+        }
+        return data;
+    }
+
+}
