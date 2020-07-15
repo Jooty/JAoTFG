f55d78f3ebd6d72dd64993866a6e6888f650249f
Jooty <compundbread@gmail.com>
2020-07-15 12:18:05 -0400

    Added CharacterBodyGrabber.

---
+++ JAoTFG/Assets/Scripts/Game/CharacterBodyGrabber.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public static class CharacterBodyGrabber
+{
+
+    public static GameObject GetCharacterBody(string bodyName)
+    {
+        var go = Resources.Load<GameObject>($"CharacterBodies/Humans/{bodyName}");
+        if (go)
+        {
+            return go;
+        }
+        else
+        {
+            Debug.LogError($"Could not find characterbody with name \"{bodyName}\"!");
+
+            return null;
+        }
+    }
+
+    public static GameObject GetCurrentCharacterBody()
+    {
+        var currentProfile = ProfileManager.currentProfile;
+        if (currentProfile == null)
+        {
+            Debug.LogError("Tried to get characterbody while current profile was null!");
+
+            return null;
+        }
+
+        var go = Resources.Load<GameObject>($"CharacterBodies/Humans/{currentProfile.preferredHumanCharacterBody}");
+        if (!go)
+        {
+            return go;
+        }
+        else
+        {
+            Debug.LogError($"Could not find characterbody with name \"{currentProfile.preferredHumanCharacterBody}\"!");
+
+            return null;
+        }
+    }
+
+    public static GameObject[] GetAllCharacterBodies()
+    {
+        return Resources.LoadAll<GameObject>("CharacterBodies/Humans");
+    }
+
+}
